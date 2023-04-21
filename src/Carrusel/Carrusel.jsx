2bc89b8f233fbc54d7c72dc3d3406d86f0ce9e73
sebassb2120 <sebassaldarriaga20@gmail.com>
2023-04-21 11:45:54 -0500

    se crea galeria

---
+++ src/Carrusel/Carrusel.jsx
@@ -0,0 +1,15 @@
+import "./Carrusel.css"
+export function Carrusel(){
+    return(
+        <>
+        <h1 className="text-center">TUTORES DE FIBONACCI.</h1>
+        <section>
+            <img src="https://firebasestorage.googleapis.com/v0/b/fibona-bbfed.appspot.com/o/Imagen%20de%20WhatsApp%202023-04-21%20a%20las%2011.19.50.jpg?alt=media&token=fb2b8e6f-4ac7-45c7-bba0-d667a3e27c3e" alt="wendy" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/fibona-bbfed.appspot.com/o/Imagen%20de%20WhatsApp%202023-04-21%20a%20las%2011.24.01.jpg?alt=media&token=b6542f0f-e59d-4ff2-8f65-5aba492df1b7" alt="sebas" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/fibona-bbfed.appspot.com/o/Imagen%20de%20WhatsApp%202023-04-21%20a%20las%2011.25.48.jpg?alt=media&token=714a5303-b16a-42fb-b2fc-4e41b879a0c1" alt="kathe" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/fibona-bbfed.appspot.com/o/Imagen%20de%20WhatsApp%202023-04-21%20a%20las%2011.39.46.jpg?alt=media&token=5f13bd34-6a51-45c4-b6f8-37c728637192" alt="yesid" />
+            <img src="" alt="" />
+        </section>
+        </>
+    )
+} 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+//IMPORTANDO BOOSTRAP
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
+import { Carrusel } from './Carrusel/Carrusel'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Carrusel/>
   </React.StrictMode>,
 )
