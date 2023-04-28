3566ea3b19c46c4ce61a4047777bceae347d4101
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-04-28 10:56:56 -0500

    formulario

--- src/Formulario/Formulario.jsx
+++ src/Formulario/Formulario.jsx
@@ -1,11 +1,13 @@
+
 export function Formulario() {
     return (
         <>
+        
             <form>
                 <div className="row">
                     <div className="col-12"></div>
                     <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon1">@</span>
+                        <span class="input-group-text" id="basic-addon1"> <i className="bi bi-person-circle"></i></span>
                         <input type="text" class="form-control" placeholder="Nombre" aria-label="Username" aria-describedby="basic-addon1"></input>
                     </div>
 
@@ -15,13 +17,13 @@ export function Formulario() {
                 <div className="row">
                     <div className="col-6">
                         <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <span class="input-group-text" id="basic-addon1"><i className="bi bi-envelope-at"></i></span>
                             <input type="text" class="form-control" placeholder="Correo" aria-label="Username" aria-describedby="basic-addon1"></input>
                         </div>
                     </div>
                     <div className="col-6">
                         <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">@</span>
+                            <span class="input-group-text" id="basic-addon1"><i className="bi bi-telephone"></i></span>
                             <input type="number" class="form-control" placeholder="Teléfono" aria-label="Username" aria-describedby="basic-addon1"></input>
                         </div>
                     </div>
@@ -30,14 +32,14 @@ export function Formulario() {
                 <div className="row">
                 <div className="col-6">
                         <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">@</span>
-                            <input type="text" class="form-control" placeholder="Correo" aria-label="Username" aria-describedby="basic-addon1"></input>
+                            <span class="input-group-text" id="basic-addon1"><i className="bi bi-calendar-check"></i></span>
+                            <input type="date" class="form-control" placeholder="Fecha de servicio" aria-label="Username" aria-describedby="basic-addon1"></input>
                         </div>
                     </div>
                     <div className="col-6">
                         <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">@</span>
-                            <input type="number" class="form-control" placeholder="Teléfono" aria-label="Username" aria-describedby="basic-addon1"></input>
+                            <span class="input-group-text" id="basic-addon1"><i className="bi bi-alarm"></i></span>
+                            <input type="text" class="form-control" placeholder="Hora de servicio" aria-label="Username" aria-describedby="basic-addon1"></input>
                         </div>
                     </div>
                 
